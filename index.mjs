2082273eb3158be591efd828920f7f3366f30e69
A.M <sazanami8menma8negi@gmail.com>
2023-09-26 13:37:55 +0900

    dynamo dbへのリクエスト

--- index.mjs
+++ index.mjs
@@ -1,36 +1,38 @@
-console.log('Loading function');
-
 import { DynamoDBDocumentClient, GetCommand} from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const ddbClient = new DynamoDBClient({ region: "ap-northeast-1" });
-const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+const dynamo = DynamoDBDocumentClient.from(ddbClient);
 
-const tablename = "tenki-to-fuku";
+const tableName = "tenki-to-fuku-city";
 
 export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-  const operation = event.operation;
-
-  if (operation == 'echo'){
-    return(event.payload);
+  try {
+    body = await dynamo.send(
+      new GetCommand({
+        TableName: tableName,
+        Key: {
+          id: event.pathParameters.id,
+        },
+      })
+    );
+    body = body.Item;
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
   }
-  else {
-    event.payload.TableName = tablename;
 
-    if (operation == 'create') {
-      var table_item = await ddbDocClient.send(new GetCommand(event.payload));
-      console.log(table_item);
-    }
-    else {
-      return (`Unknown operation: ${operation}`);
-    }
-  }
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 };
-
-/*
-以下の invoke AWS CLI コマンドを実行すると、定義した関数を呼び出すことができます
-
-aws lambda invoke --function-name LambdaFunctionOverHttps \
---payload file://lambda_echo_input.txt lambda_echo_output.txt --cli-binary-format raw-in-base64-out
-*/
